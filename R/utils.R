c93e1ea68788f73e922fd2a32ea3911ef427f72a
Leo Razoumov <slonik.az@gmail.com>
2018-02-21 09:20:12 -0500

    R/utils.R: setnames.dtb(.) - data.table::setnames(.) wrapper.

--- R/utils.R
+++ R/utils.R
@@ -12,5 +12,23 @@ is.naf <- function(x) # NA or FALSE
     is.na(x) | (x==FALSE)
 }
 
+
+#' \code{setnames.dtb}
+#'
+#' Convenience wrapper around \code{\link[data.table]{setnames}} function.
+#'
+#' @param dtb -- \code{data.table} object
+#' @param rename.map -- named array of the form \code{c('new1=old1',...)}
+#'
+#' @return The input is modified by reference, and returned (invisibly)
+#' so it can be used in compound statements.
+#' @export
+#'
+#' @examples
+setnames.dtb <- function(dtb, rename.map)
+{
+    data.table::setnames(dtb, old=unname(rename.map), new=names(rename.map))
+}
+
 ####################################################
 ## vim:tw=105:ft=R:spell:fdm=indent:fdl=0:fdi=:sw=4:
